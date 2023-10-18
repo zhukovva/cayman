385428db7fb13e4bebfd23d19549f8d424daca03
zhukovva <zhukov000@bk.ru>
2023-10-18 18:08:01 +0300

    add load tableView

--- ConfigDataXml.py
+++ ConfigDataXml.py
@@ -9,7 +9,10 @@ class ConfigData:
         self.tree = None
         self.root = None
         self.error_message = None
-        self.get_root()
+        self.config_list = []
+        self.error_check = self.get_root()
+        self.get_config_data()
+        print(self.config_list)
 
     def get_root(self):
         try:
@@ -17,9 +20,19 @@ class ConfigData:
             root = tree.getroot()
             self.tree = tree
             self.root = root
+            return True
         except XML_module.ParseError as err:
             err.msg = "Ошибка разметки файла"
             self.error_message = err.msg
+            return False
         except FileNotFoundError as err:
             err.msg = '{}'.format(err)
             self.error_message = err.msg
+            return False
+
+    def get_config_data(self):
+        if self.error_check:
+            self.config_list = []
+            for child in self.root.findall('cayman'):
+                config_dict = child.attrib
+                self.config_list.append(config_dict)
--- MyWindow.py
+++ MyWindow.py
@@ -11,15 +11,33 @@ class MyWindow(QtWidgets.QMainWindow, MainWindow.Ui_MainWindow):
         self.lineEdit_3.setInputMask("009.009.009.009;_")
         self.tabWidget.setCurrentIndex(0)
         self.config_file_path = config_file_path
-        config_data = ConfigData(self.config_file_path)
-        if config_data.tree is None:
-            self.error_window(config_data.error_message)
+        self.config_data = ConfigData(self.config_file_path)
+        if not self.config_data.error_check:
+            self.error_window(self.config_data.error_message)
+        if self.config_data.error_check:
+            self.load_tableview()
 
-
-    def error_window(self, error_message):
+    @staticmethod
+    def error_window(error_message):
         msg = QtWidgets.QMessageBox()
         msg.setIcon(QtWidgets.QMessageBox.Critical)
         msg.setText("Внимание!")
         msg.setInformativeText(error_message)
         msg.setWindowTitle("Ошибка")
         msg.exec_()
+
+    def load_tableview(self):
+        for config in self.config_data.config_list:
+            standard_item_list = []
+            for key in config.keys():
+                item = QtGui.QStandardItem()
+                item.setData(config[key], QtCore.Qt.DisplayRole)
+                standard_item_list.append(item)
+            self.new_model.appendRow(standard_item_list)
+        self.tableView.setModel(self.new_model)
+        self.tableView_2.setModel(self.new_model)
+        self.tableView.resizeColumnToContents(0)
+        self.tableView.resizeColumnToContents(1)
+        self.tableView.resizeColumnToContents(2)
+        self.tableView.resizeColumnToContents(3)
+        self.tableView_2.resizeColumnsToContents()
