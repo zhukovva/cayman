87a4776eb034b4fe9f84e4a8a79c732abfce7ebc
zhukovva <zhukov000@bk.ru>
2023-10-20 12:24:45 +0300

    add label instead tableView

--- MyWindow.py
+++ MyWindow.py
@@ -1,31 +1,61 @@
-from PyQt5 import QtWidgets, QtGui, QtCore
-import MainWindow
+from PyQt5.QtWidgets import QMainWindow, QVBoxLayout, QHBoxLayout, QWidget,\
+    QTabWidget, QGroupBox, QMessageBox, QRadioButton, QLabel, QSizePolicy
 from ConfigDataXml import ConfigData
 
 
-class MyWindow(QtWidgets.QMainWindow, MainWindow.Ui_MainWindow):
+class MyWindow(QMainWindow):
     def __init__(self, config_file_path, parent=None):
-        QtWidgets.QMainWindow.__init__(self, parent)
-        self.setupUi(self)
-        self.lineEdit_3.setInputMask("009.009.009.009;_")
-        self.tabWidget.setCurrentIndex(0)
+        QMainWindow.__init__(self, parent)
         self.config_file_path = config_file_path
         self.config_data = ConfigData(self.config_file_path)
         self.config_data_list = []
+        self.radio_button_list = []
+        self.label_list = []
+        self.group_box_list = []
+
+        self.setObjectName("MainWindow")
+        self.resize(700, 400)
+        self.central_widget = QWidget(self)
+        self.central_widget.setObjectName("central_widget")
+        self.setCentralWidget(self.central_widget)
+        self.vertical_layout_1 = QVBoxLayout(self.central_widget)
+        self.vertical_layout_1.setObjectName("vertical_layout_1")
+        self.tab_widget = QTabWidget(self.central_widget)
+        self.tab_widget.setObjectName("tabWidget")
+        self.tab_1 = QWidget()
+        self.tab_1.setObjectName("tab_1")
+        self.vertical_layout_2 = QVBoxLayout(self.tab_1)
+        self.vertical_layout_2.setObjectName("vertical_layout_2")
+
         if not self.config_data.error_check:
             self.error_window(self.config_data.error_message)
         if self.config_data.error_check:
             self.load_tableview()
-            self.radio_button_list = []
-            self.label_list = []
-            self.h_layout_list = []
-            self.v_layout = self.r_button_generator(self.new_model.rowCount())
-            self.horizontalLayout_2.addLayout(self.v_layout)
+            self.vertical_layout_3 = self.r_button_generator(len(self.config_data_list))
+            self.vertical_layout_2.addLayout(self.vertical_layout_3)
+        self.tab_widget.addTab(self.tab_1, "")
+        self.tab_2 = QWidget()
+        self.tab_2.setObjectName("tab_2")
+
+        self.tab_widget.addTab(self.tab_2, "")
+        self.vertical_layout_1.addWidget(self.tab_widget)
+        self.setCentralWidget(self.central_widget)
+        self.setStyleSheet("QMainWindow {\n"
+                           "    background-color:rgb(82, 82, 82);\n"
+                           "}\n"
+                           "QTabWidget::pane {\n"
+                           "        border-color: rgb(77,77,77);\n"
+                           "        background-color:rgb(101,101,101);\n"
+                           "        border-style: solid;\n"
+                           "        border-width: 1px;\n"
+                           "        border-radius: 6px;\n"
+                           "}\n"
+                           )
 
     @staticmethod
     def error_window(error_message):
-        msg = QtWidgets.QMessageBox()
-        msg.setIcon(QtWidgets.QMessageBox.Critical)
+        msg = QMessageBox()
+        msg.setIcon(QMessageBox.Critical)
         msg.setText("Внимание!")
         msg.setInformativeText(error_message)
         msg.setWindowTitle("Ошибка")
@@ -33,42 +63,34 @@ class MyWindow(QtWidgets.QMainWindow, MainWindow.Ui_MainWindow):
 
     def load_tableview(self):
         for config in self.config_data.config_list:
-            standard_item_list = []
             data = []
             for key in config.keys():
-                item = QtGui.QStandardItem()
-                item.setData(config[key], QtCore.Qt.DisplayRole)
-                standard_item_list.append(item)
                 data.append(config[key])
-            self.new_model.appendRow(standard_item_list)
             self.config_data_list.append(data)
-        print(self.config_data_list)
-        # self.tableView.setModel(self.new_model)
-        self.tableView_2.setModel(self.new_model)
 
     def r_button_generator(self, row_count):
         self.radio_button_list = []
         for row in range(0, row_count):
             rb_list = []
             for b_num in range(0, 4):
-                r_b = QtWidgets.QRadioButton(self.tab_1)
+                r_b = QRadioButton(self.tab_1)
                 rb_list.append(r_b)
             self.radio_button_list.append(rb_list)
 
-        self.h_layout_list = []
         i = 0
         for row in self.radio_button_list:
-            h_layout = QtWidgets.QHBoxLayout(self.tab_1)
-            label_name = QtWidgets.QLabel(self.config_data_list[i][0])
-            label_ip = QtWidgets.QLabel(self.config_data_list[i][1])
-            label_path = QtWidgets.QLabel(self.config_data_list[i][2])
-            size_policy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Expanding)
+            group_box = QGroupBox()
+            h_layout = QHBoxLayout(self.tab_1)
+            label_name = QLabel(self.config_data_list[i][0])
+            label_ip = QLabel(self.config_data_list[i][1])
+            label_path = QLabel(self.config_data_list[i][2])
+            size_policy = QSizePolicy(QSizePolicy.Expanding, QSizePolicy.Expanding)
             label_name.setSizePolicy(size_policy)
             label_ip.setSizePolicy(size_policy)
             label_path.setSizePolicy(size_policy)
-            label_name.setStyleSheet("font-size: 14px;")
-            label_ip.setStyleSheet("font-size: 14px;")
-            label_path.setStyleSheet("font-size: 14px;")
+            label_name.setStyleSheet("font-size: 14px; color: white;")
+            label_ip.setStyleSheet("font-size: 14px; color: white;")
+            label_path.setStyleSheet("font-size: 14px; color: white;")
             i = i + 1
             self.label_list.append(label_name)
             h_layout.addWidget(label_name)
@@ -76,9 +98,10 @@ class MyWindow(QtWidgets.QMainWindow, MainWindow.Ui_MainWindow):
             h_layout.addWidget(label_path)
             for button in row:
                 h_layout.addWidget(button)
-            self.h_layout_list.append(h_layout)
+            group_box.setLayout(h_layout)
+            self.group_box_list.append(group_box)
 
-        v_layout = QtWidgets.QVBoxLayout(self.tab_1)
-        for h_layout in self.h_layout_list:
-            v_layout.addLayout(h_layout)
+        v_layout = QVBoxLayout(self.tab_1)
+        for group_box in self.group_box_list:
+            v_layout.addWidget(group_box)
         return v_layout
