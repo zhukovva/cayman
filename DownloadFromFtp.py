be14d613c8a38cdcb7ae14f2a11b49d84a78c971
zhukovva <zhukov000@bk.ru>
2023-12-08 14:55:26 +0300

    download logic

--- DownloadFromFtp.py
+++ DownloadFromFtp.py
@@ -1,24 +1,67 @@
 from PyQt5.QtCore import QThread
-import ftplib
+from ftplib import FTP
+from re import findall
+from os import mkdir, path
 
 
 class DownloadFromFtp(QThread):
 
     def __init__(self, ip_address, parent=None):
         QThread.__init__(self, parent)
-        self.ftp_ip = ip_address
-        self.ftp = None
-        self.ftp_channels = []
-        self.ftp_day_directories = []
-        self.ftp_connection()
+        if ip_address:
+            self.ftp_ip = ip_address
+            self.ftp = FTP(self.ftp_ip)
+        if not ip_address:
+            raise ValueError("No IP address")
 
-    def ftp_connection(self):
-        ftp = ftplib.FTP(self.ftp_ip)
-        ftp.login()
-        ftp.cwd(ftp.nlst()[0])
-        self.ftp_channels = ftp.nlst()[2:6]
+    def get_file_list(self, path):
+        self.ftp.cwd(path)
+        files = []
+        for file in self.ftp.nlst():
+            if findall(r'\d{6}.WAV', file):
+                files.append(file)
+        return files
+
+    def get_local_directory(self, directory, name, data):
+        try:
+            mkdir(directory)
+        except WindowsError as e:
+            print(e)
+        try:
+            mkdir(f'{directory}\\{name}')
+        except WindowsError as e:
+            print(e)
+        try:
+            mkdir(f'{directory}\\{name}\\{data}')
+        except WindowsError as e:
+            print(e)
+
+    def download_files(self, files, directory, name, data):
+        path_local_file = f'{directory}\\{name}\\{data}\\'
+        for file in files:
+            file_host = path.join(f'{path_local_file}{data}_{file}')
+            if not path.isfile(file_host):
+                with open(file_host, 'wb') as local_file:
+                    self.ftp.retrbinary('RETR ' + file, local_file.write)
 
 
-ftp_object = DownloadFromFtp('192.168.27.200')
-print(ftp_object.ftp_channels)
-ftp_object.quit()
+ip = "192.168.39.200"
+node_root = 'DIGIOLOG'
+node_channel = 'CHANNEL1'
+node_data = '20231208'
+path_dir_ftp = f'{node_root}\\{node_channel}\\{node_data}'
+directory_local = 'd:\Объекты'
+ftp_name = 'Тестовый'
+
+try:
+    ftp_object = DownloadFromFtp(ip)
+    ftp_object.ftp.login()
+    file_list = ftp_object.get_file_list(path_dir_ftp)
+    ftp_object.get_local_directory(directory_local, ftp_name, node_data)
+    print(file_list)
+    ftp_object.download_files(file_list, directory_local, ftp_name, node_data)
+
+except Exception as e:
+    print(e)
+finally:
+    ftp_object.ftp.quit()
